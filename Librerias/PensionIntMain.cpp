a1c5d656d23e9858afc096e0ceb58341143064c5
ArmandoYanez <150973284+ArmandoYanez@users.noreply.github.com>
2023-11-15 17:07:16 -0600

    Pension int main
    Uso de la libreria pension dentro de un int main.

---
+++ Librerias/PensionIntMain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Pension.h"
+
+int main() {
+    // Declarar una variable de tipo Persona
+    Persona persona;
+
+    // Llamar a la función PedidadDeDatos() para obtener la información del usuario
+    persona = PedidadDeDatos();
+
+    return 0;
+}
